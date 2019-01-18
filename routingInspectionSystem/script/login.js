0165de6e29afc7df26fe5db292aa34fa84ab40be
Arvin Huang <1095369685@qq.com>
2019-01-18 18:09:24 +0800

    GPS信号强化

--- routingInspectionSystem/script/login.js
+++ routingInspectionSystem/script/login.js
@@ -1,234 +1,299 @@
 apiready = function() {
 
-var intervalTime = 5000;
-var requestTime = 2000;
+    var intervalTime = 5000;
+    var requestTime = 2000;
 
-var mainH = api.winHeight - $api.offset($api.byId("header")).h - $api.offset($api.byId("footer")).h;
-$api.byId("main").setAttribute("style", "height:" + mainH + "px;");
+    var mainH = api.winHeight - $api.offset($api.byId("header")).h - $api.offset($api.byId("footer")).h;
+    $api.byId("main").setAttribute("style", "height:" + mainH + "px;");
 
-var checkonline = true;
-api.addEventListener({
-    name: 'onlineoff'
-}, function(ret, err) {
-    if (err) {
-        alert(JSON.stringify(err));
-        return false;
-    }
-    if (checkonline) {
-        checkonline = false;
-    }
-
-});
-
-var checkBoxStuff = function(username, password) {
-    var checkb = window.$api.byId("checkbox");
-    if (checkb.checked) {
-        setPrefs(username, password);
-    }
-}
-
-var setPrefs = function(username, password) {
-    api.setPrefs({
-        key: 'userInfo',
-        value: {
-            'username': '' + username,
-            'password': '' + password
-        }
-    });
-}
-
-var aMapLBS = api.require('aMapLBS');
-var positions = [];
-var send = false;
-var showingErrMessage = false;
-var showingFailMessage = false;
-var alerts = true;
-var checkrefresh = 0;
-var notGPS = false;
-var showingmsg = 10;
-
-var startlbsPO = function(ms, ms2, userid) {
-    aMapLBS.configManager({
-        accuracy: 'kilometer',
-        filter: 0.1
+    var checkonline = true;
+    api.addEventListener({
+        name: 'onlineoff'
     }, function(ret, err) {
-        if (ret.status) {
-              setInterval(function() {
-                          var pos = positions[0];
-                          var lat = 0;
-                          var lon = 0;
-                          if (pos) {
-                              lat = pos[1];
-                              lon = pos[0];
-                          }
-                          connectToService(commonURL + "?action=position", {
-                                  values: {
-                                      "userid": userid,
-                                      "lat": lat,
-                                      "lon": lon
-                                  }
-                              },
-                              function(ret) {
-                                  if (ret.result == true) {
+        if (err) {
+            alert(JSON.stringify(err));
+            return false;
+        }
+        if (checkonline) {
+            checkonline = false;
+        }
 
-                                  } else {
-                                      if (!showingFailMessage) {
-                                          showingErrMessage = true;
-                                          // alert("当前网络信号不佳， 无法进行实时定位！");
-                                      }
-                                  }
-                              },
-                              function(ret) {
-                                  if (!showingErrMessage) {
-                                      showingErrMessage = true;
-                                      // alert("程序错误，程序员正在奋斗中！");
-                                  }
-                              });
-              }, ms2);
+    });
 
-    // setInterval(function(){
-    // 		aMapLBS.singleLocation({
-    // 				timeout: 5
-    // 		}, function(ret, err) {
-    // 				if (ret.status) {
-    // 					//TODO：此处记录一个位置信息信息内容，并发送位置信息。信息
-    // 					if(positions.length >= 50){
-    // 						positions.pop();
-    // 					}
-    // 					positions.unshift([ret.lon, ret.lat]);
-    //           // alert(JSON.stringify(positions));
-    // 					$api.setStorage('position', positions);
-    //           alert($api.getStorage("position"));
-    // 					if(!send)
-    // 					{
-    // 						sned = true;
-    // 					}
-    // 					else {
-    // 						//发送请求。
-    // 					}
-    // 				}
-    // 			});
-    // 		},ms);
-    //通过LBS进行连续定位
-    // api.addEventListener({
-    //     name: 'changePositionList'
-    // }, function(ret, err){
-    //     if( ret ){
-    //       var v = ret.value;
-    //       positions = [];
-    //       positions.unshift([v.lon, v.lat]);
-    //     }else{
-    //       // alert( JSON.stringify( err ) );
-    //     }
-    // });
-        aMapLBS.stopUpdatingLocation();
-        aMapLBS.startLocation(function(ret, err) {
-            if (ret.status) {
-                if (positions.length >= 100) {
-                    positions.pop();
-                }
-                positions.unshift([ret.lon, ret.lat]);
-                $api.setStorage('position', JSON.stringify(positions));
-                checkrefresh ++;
-                if(checkrefresh >= 2){
-                  checkrefresh = 0 ;
-                  api.sendEvent({
-                      name: 'refreshmap'
-                  });
-                }
-                // alert($api.getStorage("position"));
+    var checkBoxStuff = function(username, password) {
+        var checkb = window.$api.byId("checkbox");
+        if (checkb.checked) {
+            setPrefs(username, password);
+        }
+    }
 
-            } else {
-                // alert("当前无法进行定位。");
+    var setPrefs = function(username, password) {
+        api.setPrefs({
+            key: 'userInfo',
+            value: {
+                'username': '' + username,
+                'password': '' + password
             }
         });
     }
-});
 
-// mapObj = new AMap.Map('iCenter');
-// mapObj.plugin('AMap.Geolocation', function() {
-//     geolocation = new AMap.Geolocation({
-//         enableHighAccuracy: false, //是否使用高精度定位，默认:true
-//         timeout: 5000,
-//         convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-//         showButton: false, //显示定位按钮，默认：true
-//         buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-//         showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
-//         showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
-//         panToLocation: false, //定位成功后将定位到的位置作为地图中心点，默认：true
-//         zoomToAccuracy: false //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-//     });
-//     mapObj.addControl(geolocation);
-//     AMap.event.addListener(geolocation, 'complete', function(data) {
-//         if(notGPS){
-//             notGPS = false;
-//         }
-//         showingmsg = 5;
-//         var ret = data.position;
-//         if (positions.length >= 100) {
-//             positions.pop();
-//         }
-//         positions.unshift([ret.lng, ret.lat]);
-//         $api.setStorage('position', JSON.stringify(positions));
-//         checkrefresh++;
-//         if (checkrefresh >= 2) {
-//             checkrefresh = 0;
-//             api.sendEvent({
-//                 name: 'refreshmap'
-//             });
-//         }
-//     }); //返回定位信息
-//     AMap.event.addListener(geolocation, 'error', function() {
-//         if(notGPS){
-//           return ;
-//         }
-//         if(showingmsg == 10){
-//           alert("当前GPS信号不佳");
-//           showingmsg = 0;
-//         }
-//         else {
-//           showingmsg ++;
-//         }
-//         notGPS = true;
-//     }); //返回定位出错信息
-//     setInterval(function() {
-//         geolocation.getCurrentPosition(function(status, result) {
-//             if (status == "complete") {
-//                 var pos = positions[0];
-//                 var lat = 0;
-//                 var lon = 0;
-//                 if (pos) {
-//                     lat = pos[1];
-//                     lon = pos[0];
-//                 }
-//                 connectToService(commonURL + "?action=position", {
-//                         values: {
-//                             "userid": userid,
-//                             "lat": lat,
-//                             "lon": lon
-//                         }
-//                     },
-//                     function(ret) {
-//                         if (ret.result == true) {
-//
-//                         } else {
-//                             if (!showingFailMessage) {
-//                                 showingErrMessage = true;
-//                                 // alert("当前网络信号不佳， 无法进行实时定位！");
-//                             }
-//                         }
-//                     },
-//                     function(ret) {
-//                         if (!showingErrMessage) {
-//                             showingErrMessage = true;
-//                             // alert("程序错误，程序员正在奋斗中！");
-//                         }
-//                     });
-//             }
-//         })
-//     }, ms2);
-//
-// });
+    var aMapLBS = api.require('aMapLBS');
+    // var aMapReportLocation = api.require('aMapReportLocation');
+    // var aMap = api.require('aMap');
+    var positions = [];
+    var send = false;
+    var showingErrMessage = false;
+    var showingFailMessage = false;
+    var alerts = true;
+    var checkrefresh = 0;
+    var notGPS = false;
+    var showingmsg = 10;
+
+    var sendPos = function(ms2, userid) {
+            setInterval(function() {
+                var pos = positions[0];
+                var lat = 0;
+                var lon = 0;
+                if (pos) {
+                    lat = pos[1];
+                    lon = pos[0];
+                }
+                connectToService(commonURL + "?action=position", {
+                        values: {
+                            "userid": userid,
+                            "lat": lat,
+                            "lon": lon
+                        }
+                    },
+                    function(ret) {
+                        if (ret.result == true) {
+
+                        } else {
+                            if (!showingFailMessage) {
+                                showingErrMessage = true;
+                                // alert("当前网络信号不佳， 无法进行实时定位！");
+                            }
+                        }
+                    },
+                    function(ret) {
+                        if (!showingErrMessage) {
+                            showingErrMessage = true;
+                            // alert("程序错误，程序员正在奋斗中！");
+                        }
+                    });
+            }, ms2);
+        }
+        //
+        var startlbsPO = function(ms, ms2, userid) {
+        //
+        //
+        //     /********* amap 插件 ************/
+        //     aMap.open({
+        //         rect: {
+        //             x: 0,
+        //             y: 0,
+        //             w: 1,
+        //             h: 1
+        //         },
+        //         showUserLocation: true,
+        //         zoomLevel: 11,
+        //     }, function(ret, err) {
+        //         if (ret.status) {
+        //           sendPos(ms2, userid);
+        //           aMap.stopLocation();
+        //           aMap.getLocation({
+        //               autoStop: false
+        //           },function(ret, err) {
+        //               if (ret.status) {
+        //                   if (positions.length >= 100) {
+        //                       positions.pop();
+        //                   }
+        //                   if(ret.accuracy <= 100){
+        //                       positions.unshift([ret.lon, ret.lat]);
+        //                       $api.setStorage('position', JSON.stringify(positions));
+        //                       api.sendEvent({
+        //                           name: 'refreshmap'
+        //                       });
+        //                   }
+        //               } else {
+        //                   alert(JSON.stringify(err));
+        //               }
+        //           });
+        //         }
+        //     });
+
+    aMapLBS.configManager({
+        accuracy: 'kilometer',
+        filter: 1
+    }, function(ret, err) {
+        if (ret.status) {
+            sendPos(ms2);
+            // setInterval(function(){
+            // 		aMapLBS.singleLocation({
+            // 				timeout: 5
+            // 		}, function(ret, err) {
+            // 				if (ret.status) {
+            // 					//TODO：此处记录一个位置信息信息内容，并发送位置信息。信息
+            // 					if(positions.length >= 50){
+            // 						positions.pop();
+            // 					}
+            // 					positions.unshift([ret.lon, ret.lat]);
+            //           // alert(JSON.stringify(positions));
+            // 					$api.setStorage('position', positions);
+            //           alert($api.getStorage("position"));
+            // 					if(!send)
+            // 					{
+            // 						sned = true;
+            // 					}
+            // 					else {
+            // 						//发送请求。
+            // 					}
+            // 				}
+            // 			});
+            // 		},ms);
+            //通过LBS进行连续定位
+            // api.addEventListener({
+            //     name: 'changePositionList'
+            // }, function(ret, err){
+            //     if( ret ){
+            //       var v = ret.value;
+            //       positions = [];
+            //       positions.unshift([v.lon, v.lat]);
+            //     }else{
+            //       // alert( JSON.stringify( err ) );
+            //     }
+            // });
+            aMapLBS.stopUpdatingLocation();
+            aMapLBS.startLocation(function(ret, err) {
+                if (ret.status) {
+                    if (positions.length >= 100) {
+                        positions.pop();
+                    }
+                    positions.unshift([ret.lon, ret.lat]);
+                    $api.setStorage('position', JSON.stringify(positions));
+                    api.sendEvent({
+                        name: 'refreshmap'
+                    });
+                    // alert($api.getStorage("position"));
+                } else {
+                    // alert("当前无法进行定位。");
+                }
+            });
+
+            /********** amapReportLocation **********/
+            // aMapReportLocation.stopLocation();
+            // aMapReportLocation.startLocation({
+            //     accuracy: 'hight_accuracy',
+            //     filter: 1,
+            //     autoStop: false,
+            //     report: {
+            //         uid: '123456789',
+            //         url: "https://",
+            //         interval: 30,
+            //         type: 'post',
+            //         headerField: {
+            //            timestamp: '',
+            //            ContentType: 'text/plain',
+            //            custid: '123456',
+            //            Accept: '*/*'
+            //          },
+            //          aesKey: 'apicloud'
+            //     }
+            // }, function(ret) {
+            //     if (ret.status) {
+            //         api.alert({ msg: JSON.stringify(ret) });
+            //     } else {
+            //         api.alert({ msg: '定位失败' });
+            //     }
+            // });
+        }
+    });
+
+    /*********** 高德JS 原生 ************/
+    // mapObj = new AMap.Map('iCenter');
+    // mapObj.plugin('AMap.Geolocation', function() {
+    //     geolocation = new AMap.Geolocation({
+    //         enableHighAccuracy: false, //是否使用高精度定位，默认:true
+    //         timeout: 5000,
+    //         convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+    //         showButton: false, //显示定位按钮，默认：true
+    //         buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+    //         showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
+    //         showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
+    //         panToLocation: false, //定位成功后将定位到的位置作为地图中心点，默认：true
+    //         zoomToAccuracy: false //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+    //     });
+    //     mapObj.addControl(geolocation);
+    //     AMap.event.addListener(geolocation, 'complete', function(data) {
+    //         if(notGPS){
+    //             notGPS = false;
+    //         }
+    //         showingmsg = 5;
+    //         var ret = data.position;
+    //         if (positions.length >= 100) {
+    //             positions.pop();
+    //         }
+    //         positions.unshift([ret.lng, ret.lat]);
+    //         $api.setStorage('position', JSON.stringify(positions));
+    //         checkrefresh++;
+    //         if (checkrefresh >= 2) {
+    //             checkrefresh = 0;
+    //             api.sendEvent({
+    //                 name: 'refreshmap'
+    //             });
+    //         }
+    //     }); //返回定位信息
+    //     AMap.event.addListener(geolocation, 'error', function() {
+    //         if(notGPS){
+    //           return ;
+    //         }
+    //         if(showingmsg == 10){
+    //           alert("当前GPS信号不佳");
+    //           showingmsg = 0;
+    //         }
+    //         else {
+    //           showingmsg ++;
+    //         }
+    //         notGPS = true;
+    //     }); //返回定位出错信息
+    //     setInterval(function() {
+    //         geolocation.getCurrentPosition(function(status, result) {
+    //             if (status == "complete") {
+    //                 var pos = positions[0];
+    //                 var lat = 0;
+    //                 var lon = 0;
+    //                 if (pos) {
+    //                     lat = pos[1];
+    //                     lon = pos[0];
+    //                 }
+    //                 connectToService(commonURL + "?action=position", {
+    //                         values: {
+    //                             "userid": userid,
+    //                             "lat": lat,
+    //                             "lon": lon
+    //                         }
+    //                     },
+    //                     function(ret) {
+    //                         if (ret.result == true) {
+    //
+    //                         } else {
+    //                             if (!showingFailMessage) {
+    //                                 showingErrMessage = true;
+    //                                 // alert("当前网络信号不佳， 无法进行实时定位！");
+    //                             }
+    //                         }
+    //                     },
+    //                     function(ret) {
+    //                         if (!showingErrMessage) {
+    //                             showingErrMessage = true;
+    //                             // alert("程序错误，程序员正在奋斗中！");
+    //                         }
+    //                     });
+    //             }
+    //         })
+    //     }, ms2);
+    //
+    // });
 }
 
 var loginfunc = function(username, password) {
